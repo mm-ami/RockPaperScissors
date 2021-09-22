56cc8ec81ac8b2da468897db57124b1707711e22
mm-ami <mhju.9599@gmail.com>
2021-09-22 10:46:46 +0900

    update:index.html main.js style.css

--- main.js
+++ main.js
@@ -15,10 +15,115 @@ const game = function() {
     });
   };
 
-  
+  //判定にもとづいた画面表示
+  const Gamestr = () => {
+    const options = document.querySelectorAll('.select button');
+    const Playerhand = document.querySelector('.player-hand');
+    const Computerhand = document.querySelector('.computer-hand');
+    const hands = document.querySelectorAll('.hands img');
+
+    hands.forEach(hand => {
+      hand.addEventListener('animationend',function() {
+        this.style.animation = "";
+      });
+    });
+
+    const rundom = ['グー','チョキ','パー'];
+
+    options.forEach(option => {
+      option.addEventListener('click', function(){
+        //コンピュータのランダム表示
+        const rundomOption = Math.floor(Math.random()*3);
+        const computerChoice = rundom[rundomOption];
+
+        setTimeout(() => {
+          computerchoice(this.textContent,computerChoice);
+
+          Playerhand.src = `./picture/${this.textContent}.png`;
+          Computerhand.src = `./picture/${computerChoice}.png`;
+        },1000);
+
+        Playerhand.style.animation = "shakePlayer 1s ease";
+        Computerhand.style.animation = "shakeComputer 1s ease";
+      });
+    });
+  }
+
+  const updatescore = () => {
+    const PlayerScore = document.querySelector('.player-score p');
+    const ComputerScore = document.querySelector('.computer-score p');
+    PlayerScore.textContent = pscore;
+    ComputerScore.textContent = cscore;
+
+    if(PlayerScore.textContent === 5) {
+      choice.textContent = 'あなたの勝利！';
+      return;
+    } else if(ComputerScore.textContent === 5) {
+      choice.textContent = 'コンピュータの勝利！';
+      return;
+    }
+  }
+
+  //じゃんけんの判定
+  const computerchoice = (playerChoice,computerChoice) => {
+    const choice =  document.querySelector('.choice');
+    if(playerChoice === computerChoice) {
+      choice.textContent = '引き分け';
+      return;
+    };
+    if(playerChoice === 'グー') {
+      if(computerChoice === 'チョキ') {
+        choice.textContent = 'あなたの勝ち';
+        pscore++;
+        updatescore();
+        return;
+      } else {
+        choice.textContent = 'コンピュータの勝ち';
+        cscore++;
+        updatescore();
+        return;
+      };
+    }
+    if(playerChoice === 'チョキ') {
+      if(computerChoice === 'パー') {
+        choice.textContent = 'あなたの勝ち';
+        pscore++;
+        updatescore();
+        return;
+      } else {
+        choice.textContent = 'コンピュータの勝ち';
+        cscore++;
+        updatescore();
+        return;
+      };
+    }
+    if(playerChoice === 'パー') {
+      if(computerChoice === 'グー') {
+        choice.textContent = 'あなたの勝ち';
+        pscore++;
+        updatescore();
+        return;
+      } else {
+        choice.textContent = 'コンピュータの勝ち';
+        cscore++;
+        updatescore();
+        return;
+      }
+    }
+
+    if(PlayerScore.textContent === 5) {
+      choice.textContent = 'あなたの勝利！';
+    } else if(ComputerScore.textContent === 5) {
+      choice.textContent = 'コンピュータの勝利！';
+    }
+  };
 
   //関数実行
   start();
+  Gamestr();
 };
 
 game();
+
+
+//戻るボタンを押したら、数字がリセットされるようにする
